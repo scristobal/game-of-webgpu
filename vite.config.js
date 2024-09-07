859db761fe11c87a7aeb35a0eea3754a99b0a142
Samuel Cristobal <samuel.cristobal@protonmail.com>
2024-09-07 20:50:36 +0200

    Fix dev dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-/* eslint-disable no-undef */
 import * as child from 'node:child_process';
 import { defineConfig } from 'vite';
-import { resolve } from 'node:path';
 
 const commitHash = child.execSync('git rev-parse --short HEAD').toString();
 
 // https://vitejs.dev/config/
 export default defineConfig({
     define: { 'import.meta.env.VITE_APP_VERSION': JSON.stringify(commitHash) },
-    build: {
-        target: 'esnext',
-    },
+    build: { target: 'esnext' }
 });
