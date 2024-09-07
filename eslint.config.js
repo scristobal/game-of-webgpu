257877a304a595c38e22e58c01a7ce022d5c0445
Samuel Cristobal <samuel.cristobal@protonmail.com>
2024-09-07 19:25:02 +0200

    Code migrated from webgpu-experiments repo

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+
+export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
+    languageOptions: {
+        ecmaVersion: 'latest',
+        globals: { ...globals.browser }
+    }
+});
