deffcefc34f5845dd6fde0ead178bdd1b135126b
Samuel Cristobal <samuel.cristobal@protonmail.com>
2025-11-11 07:28:44 +0100

    Reorg code folders

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // Adapted from https://codelabs.developers.google.com/your-first-webgpu-app
-import cellShader from './cell.wgsl?raw';
-import simulationShader from './simulation.wgsl?raw';
+import cellShader from './shaders/cell.wgsl?raw';
+import simulationShader from './shaders/simulation.wgsl?raw';
 
 async function gameOfWebGPU() {
     // initialization and checks
